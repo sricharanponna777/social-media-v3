ad3b22af99d946fa65007f429cb3ee536b60c011
Sricharan Ponna <sricharanponna777@gmail.com>
2025-08-29 18:19:51 +0100

    Friends functionality works

---
+++ app/(main)/(chats)/main.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Chats() {
+  return (
+    <View>
+      <Text className='text-lg text-foreground'>My Chats</Text>
+    </View>
+  )
+}
